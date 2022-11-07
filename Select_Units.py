2b7b133bd5d3a2f989566ded4d59d998b24aecdd
Bugs <r1ch404ps@gmail.com>
2022-11-07 15:28:48 +0000

    Add Commentary to Select_Units

--- Select_Units.py
+++ Select_Units.py
@@ -1,8 +1,13 @@
+#DEFINE FUNCTION WHICH RETURNS A LIST OF OBJECTS OF THE DESIRED CLASS
 def Select_Units(Class, Name):
     
+    #CREATE EMPTY LIST TO STORE THE OBJECTS
     Units_List = []
     
+    #ITERATE IN A RANGE FROM 0 TO X (NUMBER CHOSEN BY THE PLAYER)
     for i in range(0,int(input("Number of " + Name + ": "))):
+        #APPEND X OBJECTS OF THE DESIRED CLASS TO THE LIST
         Units_List.append(Class)
-        
+    
+    #RETURN THE LIST OF OBJECTS OF THE DESIRED CLASS    
     return Units_List
