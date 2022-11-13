663b8e8646f0dd99db86481c0b0ae780a853b5c8
Ricardo Louro <ricardo.f.louro@gmail.com>
2022-11-13 19:31:26 +0000

    Fix issue with function not creating new instances but rather copying the same instance

--- Select_Units.py
+++ Select_Units.py
@@ -1,19 +1,36 @@
+#IMPORTS
+from Unit_Classes import *
+
 #DEFINE FUNCTION WHICH RETURNS A LIST OF OBJECTS OF THE DESIRED CLASS
-def Select_Units(Class, Name):
+def Select_Units(ClassName):
     
     #CREATE EMPTY LIST TO STORE THE OBJECTS
     Units_List = []
     
-    #ITERATE IN A RANGE FROM 0 TO X (NUMBER CHOSEN BY THE PLAYER)
-    if Name == "Orc Warriors":
-        for i in range(0,int(input("How many " + Name + " will appear? "))):
-            #APPEND X OBJECTS OF THE DESIRED CLASS TO THE LIST
-            Units_List.append(Class)
+    #INITIATE NUMBER SO WE CAN REPRESENT THE DIFFERENT OBJECTS OF THE SAME TYPE
+    Number = 0
+
+    #IF THE DESIRED CLASS IS THE ORC_WARRIOR
+    if ClassName == "Orc Warrior_":
+        #ITERATE IN A RANGE FROM 0 TO X (NUMBER CHOSEN BY THE PLAYER)
+        for i in range(0,int(input("How many Orc Warriors will appear? "))):
+            #APPEND X OBJECTS OF THE ORC_WARRIOR CLASS TO THE LIST
+            Units_List.append(Orc_Warrior())
+
+    #IF THE DESIRED CLASS IS THE WARRIOR
+    elif ClassName == "Warrior_":
+        #ITERATE IN A RANGE FROM 0 TO X (NUMBER CHOSEN BY THE PLAYER)
+        for i in range(0,int(input("How many Warriors are in the party? "))):
+            #APPEND X OBJECTS OF THE WARRIOR CLASS TO THE LIST
+            Units_List.append(Warrior())
+
+    #IF THE DESIRED CLASS IS THE PRIEST
     else:
-        for i in range(0,int(input("How many " + Name + " are in the party? "))):
-            #APPEND X OBJECTS OF THE DESIRED CLASS TO THE LIST
-            Units_List.append(Class)
-            
+        #ITERATE IN A RANGE FROM 0 TO X (NUMBER CHOSEN BY THE PLAYER)
+        for i in range(0,int(input("How many Priests are in the party? "))):
+            #APPEND X OBJECTS OF THE PRIEST CLASS TO THE LIST
+            Units_List.append(Priest())
+
     print("")
     
     #RETURN THE LIST OF OBJECTS OF THE DESIRED CLASS    
