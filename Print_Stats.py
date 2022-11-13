8167257b44b64d82eef7eb2395a05be9ca604ab8
Ricardo Louro <ricardo.f.louro@gmail.com>
2022-11-13 19:50:37 +0000

    Add Print_Stats. Prints main values of the object given.

---
+++ Print_Stats.py
@@ -0,0 +1,9 @@
+#IMPORTS
+from Unit_Classes import *
+
+#PRINTS THE GIVEN UNIT'S STATS
+def Print_Stats(Unit):
+    print("   HP: " + str(Unit.HP))
+    print("   MP: " + str(Unit.MP))
+    print("   WP: " + str(Unit.WP))
+    print("")
