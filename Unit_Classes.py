0a55bca48a3d4bfdcde8ce381c174d799aec3ea2
Ricardo Louro <ricardo.f.louro@gmail.com>
2022-11-13 19:33:16 +0000

    Add returns to the Class Methods so I can call SpellEffectValue and SpellMPCost

--- Unit_Classes.py
+++ Unit_Classes.py
@@ -4,6 +4,7 @@ import random
 #DEFINING THE WARRIOR CLASS WITH THE DEFAULT VALUES AND ITS SPELLS
 class Warrior():
     def __init__(self):
+        self.Name = "Warrior_"
         self.HP = 32
         self.MP = 5
         self.AP = 2
@@ -14,10 +15,12 @@ class Warrior():
     def Rushdown(self):
         SpellEffectValue = -1 * (self.WP + random.randint(1,4))
         SpellMPCost = 5
+        return (SpellEffectValue, SpellMPCost)
 
 #DEFINING THE PRIEST CLASS WITH THE DEFAULT VALUES AND ITS SPELLS
 class Priest():
     def __init__(self):
+        self.Name = "Priest_"
         self.HP = 20
         self.MP = 25
         self.AP = 0
@@ -25,18 +28,21 @@ class Priest():
         self.Init = 6
     
     #DEFININING A METHOD FOR THE SPELL SO WE CAN CALL IT WHENEVER WE NEED
-    def Exorcism():
+    def Exorcism(self):
         SpellEffectValue = -1 * (random.randint(1,4) * 2)
-        SpellMPCost: 5
+        SpellMPCost = 5
+        return (SpellEffectValue, SpellMPCost)
     
     #DEFININING A METHOD FOR THE SPELL SO WE CAN CALL IT WHENEVER WE NEED
     def Mend(self):
         SpellEffectValue = (random.randint(1,6) + self.WP)
         SpellMPCost = 3
+        return (SpellEffectValue, SpellMPCost)
 
 #DEFININING THE ENEMY'S CHARACTER CLASS WITH THE DEFAULT VALUES
 class Orc_Warrior():
     def __init__(self):
+        self.Name = "Orc Warrior_"
         self.HP = 15
         self.MP = 0
         self.AP = 2
